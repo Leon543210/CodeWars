9b1e30a8bef4b6439adfcda1a3753a0f5155517e
Ruben Green <greenrl91@gmail.com>
2023-10-19 19:21:49 -0400

    Updating old js files, new 'Summation-of-1-to-num;

---
+++ 8KYU/Count-of-Positives-Sum-Of-Negatives.js
@@ -0,0 +1,21 @@
+console.log();
+
+let randArray = [3, 4, 3, 4, 5, -10, -20, -10]
+let positive = 0;
+let negative = 0;
+let newNeg;
+let rand;
+
+randArray.forEach(rand => {
+    /* if(rand >= 0){
+        positive++;
+        console.log(positive)
+    } */
+    
+    
+});
+if(rand <= 0){
+        newNeg = randArray[rand];
+        newNeg = new rand;
+        console.log(newNeg);
+    }
