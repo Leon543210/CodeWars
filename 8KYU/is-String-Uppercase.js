1aa59c6e4492f73850d98ff81e673892049fe4cc
Ruben Green <greenrl91@gmail.com>
2024-01-09 18:56:32 -0500

    Committing challenge information

--- 8KYU/is-String-Uppercase.js
+++ 8KYU/is-String-Uppercase.js
@@ -1,3 +1,17 @@
+/*
+    Title: Small enough? - Beginner
+
+    Description:
+        Task
+        Create a method to see whether the string is ALL CAPS.
+
+        In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter
+        so any string containing no letters at all is trivially considered to be in ALL CAPS.
+
+        Source: https://www.codewars.com/kata/56cd44e1aa4ac7879200010b/train/javascript
+*/
+
+
 let inputOne = 'test'
 let inputTwo = inputOne.toUpperCase();
 
