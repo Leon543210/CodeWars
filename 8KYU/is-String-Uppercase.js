7176ec538223941daba644712445d01f07673f1b
Ruben Green <greenrl91@gmail.com>
2024-01-09 18:52:35 -0500

    Committing latest challenges

---
+++ 8KYU/is-String-Uppercase.js
@@ -0,0 +1,12 @@
+let inputOne = 'test'
+let inputTwo = inputOne.toUpperCase();
+
+function upper(inputOne){
+    //console.log(inputTwo.toUpperCase())
+
+    if(inputOne !== inputTwo){
+        return false;
+    }else{
+        return true;
+    }
+}
