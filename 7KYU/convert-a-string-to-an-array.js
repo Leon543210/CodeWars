e32a1062c492d61d268deab964699ff33b91ab18
Ruben Green <greenrl91@gmail.com>
2024-01-11 12:36:56 -0500

    Committing latest successful challenge

---
+++ 7KYU/convert-a-string-to-an-array.js
@@ -0,0 +1,15 @@
+/*
+    Title: Convert a string to an array
+
+    Description: 
+
+    Write a function to split a string and convert it into an array of words.
+
+    Source: https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/javascript
+*/
+
+
+
+function splitString(str){
+    return str.split(' ');
+}
