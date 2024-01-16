7f8a68d7179e9d5f41be881852152107cf8bb047
Ruben Green <greenrl91@gmail.com>
2024-01-16 00:07:42 -0500

    Updating submitted challenge - Details, Code;

--- 8KYU/multTable.js
+++ 8KYU/multTable.js
@@ -1,10 +1,22 @@
+/*
+    Title: Multiplication table for number
+
+    Description:
+        Your goal is to return multiplication table for number that is always an integer from 1 to 10.
+
+        P. S. You can use \n in string to jump to the next line.
+
+        Note: newlines should be added between rows, but there should be no trailing newline at the end. 
+            If you're unsure about the format, look at the sample tests.
+
+    Source: https://www.codewars.com/kata/5a2fd38b55519ed98f0000ce/javascript
+*/
+
 function multTable(number){
-    let tracker = ''
+    let tracker = []
     for(let i = 1; i <= 10; i++){
         let result = i * number;
-        tracker += `${i} * ${number} = ${result}\n`
+        tracker.push(`${i} * ${number} = ${result}`)
          
-    }return tracker + "\n";
+    }return tracker.join('\n');
 }
-
-//console.log(`${i} * ${number} = ${result}`);
