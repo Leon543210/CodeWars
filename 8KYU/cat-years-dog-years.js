db7ba6515d0ad4f6f68cd0f958d5f93a060a0399
Ruben Green <greenrl91@gmail.com>
2024-01-19 21:09:26 -0500

    Committing latest successful challenge

---
+++ 8KYU/cat-years-dog-years.js
@@ -0,0 +1,47 @@
+/*
+    Title: Cat years, Dog years
+
+    Description:
+
+        I have a cat and a dog. I got them at the same time as kitten/puppy. That was humanYears years ago.
+        Return their respective ages now as [humanYears,catYears,dogYears]
+
+        NOTES:
+
+            humanYears >= 1
+            humanYears are whole numbers only
+        
+        Cat Years
+            15 cat years for first year
+            +9 cat years for second year
+            +4 cat years for each year after that
+        
+        Dog Years
+            15 dog years for first year
+            +9 dog years for second year
+            +5 dog years for each year after that
+        
+        References
+            http://www.catster.com/cats-101/calculate-cat-age-in-cat-years
+            http://www.slate.com/articles/news_and_politics/explainer/2009/05/a_dogs_life.html
+
+
+    Source: https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b/javascript
+*/
+function petAge(humanYears){
+    let catYears = 15;
+    let dogYears = 15;
+    if(humanYears == 2){
+        catYears = catYears + 9;
+        dogYears = dogYears + 9;
+    } else if(humanYears >= 3){
+        catYears = 24 + ((humanYears - 2)  * 4)
+        dogYears = 24 + ((humanYears - 2)  * 5)
+    }
+    return [humanYears, catYears, dogYears];
+}
+/*
+    Additional Solutions:
+
+
+*/
