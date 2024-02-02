01aa20f84e66a1178ebd8fd658812a6be23f51c3
Ruben Green <greenrl91@gmail.com>
2024-02-01 23:57:31 -0500

    [unfinished challenge]

---
+++ 8KYU/high-and-low.js
@@ -0,0 +1,17 @@
+/*
+    Title: Highest and Lowest
+
+    Description:
+
+        In this little assignment you are given a string of space separated numbers, 
+        and have to return the highest and lowest number.
+
+    Source: https://www.codewars.com/kata/554b4ac871d6813a03000035/javascript
+*/
+
+function highAndLow(arr){
+    
+    numbers = numbers.split(' ');
+    return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+}
+ 
