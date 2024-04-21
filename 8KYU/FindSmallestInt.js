53d34b64df93eba16efaa00bc3a115919d9498aa
Ruben Green <greenrl91@gmail.com>
2024-04-20 22:36:59 -0400

    Latest 8KYU Codewars Challenge

---
+++ 8KYU/FindSmallestInt.js
@@ -0,0 +1,20 @@
+/*
+    Title: Find the smallest integer in the array
+
+    Description:
+
+        Given an array of integers your solution should find the smallest integer.
+
+        For example:
+
+            Given [34, 15, 88, 2] your solution will return 2
+            Given [34, -345, -1, 100] your solution will return -345
+        You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+    Source: https://www.codewars.com/kata/55a2d7ebe362935a210000b2/javascript
+*/
+
+function smallest(arr){
+    // return Math.min(...arr)
+    console.log(Math.min(...arr))
+  }
